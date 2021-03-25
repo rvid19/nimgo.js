d1473b54210e4cb051fabecda1ecbff6ec8a83f5
rvid19 <arvid@vmail.se>
2021-03-25 19:40:53 +0000

    added project dependencies and dynamic html attibutes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("hello world!");
+//nimgo query
+function nq(query,all) {
+    if (all) {
+        return document.querySelectorAll(query);
+    } else {
+        return document.querySelector(query);
+    }
+}
+// nimgo dependencies
+var nd = {
+    scope: {},
+    get http() {
+        console.warn("The http dependency currently doesn't work, but it should work in a future release.")
+    }
+};
+
+function nimgo(deps, call) {
+    var list = [];
+    for (const dep of deps) {
+        if (!(dep in nd)) throw new Error(`Unknown dependency: ${dep}`);
+        list.push(nd[dep]);
+    }
+    call(...list);
+}
+
+window.onload = () => {
+    a = setInterval(() => {
+        for (const elem of nq("*",true)) {
+            var list = Array.from(elem.attributes)
+            .filter(a => a.nodeName.startsWith("*"));
+
+            for (const attr of list) {
+                elem[attr.nodeName.substring(1)] = replace(attr.nodeValue);
+            }
+        }
+    }, 10);
+}
+/*-----------------------------------------utils-----------------------------------------*/
+function replace(str) {
+    for (const match of str.matchAll("{{[^{}]+}}")) {
+        str = str.replace(
+            match[0],
+            nd.scope[match[0].replace(/{|}/g, "")] || "none"
+        );
+    }
+    return str;
+}
